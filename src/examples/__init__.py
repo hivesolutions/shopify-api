7cb990b32235be6d524ee0f7d3401aef5f97bbd0
joamag <joamag@gmail.com>
2014-04-24 23:50:28 +0100

    new init file

--- src/examples/__init__.py
+++ src/examples/__init__.py
@@ -34,6 +34,8 @@ __copyright__ = "Copyright (c) 2008-2014 Hive Solutions Lda."
 __license__ = "GNU General Public License (GPL), Version 3"
 """ The license for the module """
 
-import base
+from . import base
+from . import order
 
-from base import *
+from .base import *
+from .order import *
