3d34317c344852f4deb146053885c45271f84061
joamag <joamag@hive.pt>
2020-05-18 15:15:46 +0100

    Version 0.2.0

--- setup.py
+++ setup.py
@@ -42,7 +42,7 @@ import setuptools
 
 setuptools.setup(
     name = "shopify_api",
-    version = "0.1.11",
+    version = "0.2.0",
     author = "Hive Solutions Lda.",
     author_email = "development@hive.pt",
     description = "Shopify API Client",
